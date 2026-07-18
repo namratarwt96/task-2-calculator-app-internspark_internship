256fb74da8377bbdd04f5140eb3836c9b5590637
Namrata Rawat <namratarwt96@gmail.com>
2026-07-18 17:30:52 +0530

    Update script.js git add Calculator App Functionality Logic using JavaScript

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-# Functional Calculator App Logic
+const display = document.querySelector(".display");
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach(button => {
+    button.addEventListener("click", () => {
+        const value = button.textContent;
+
+        if (value === "C") {
+            display.value = "";
+        }
+        else if (value === "⌫") {
+            display.value = display.value.slice(0, -1);
+        }
+        else if (value === "=") {
+            try {
+                display.value = eval(display.value);
+            } catch {
+                display.value = "Error";
+            }
+        }
+        else {
+            display.value += value;
+        }
+    });
+});
